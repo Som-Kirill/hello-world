4ee30547916f3b95116e70f0c35a255c18161a4d
Kirill <somkirill76@gmail.com>
2023-11-24 10:11:48 +0200

    Додана функція calcMult

--- scripts.js
+++ scripts.js
@@ -5,3 +5,8 @@ function getArguments() {
 
     return { num1, num2 };
 }
+function calcMult() {
+    const { num1, num2 } = getArguments();
+    const mult = num1 * num2;
+    console.log(`Product: ${mult}`);
+}
